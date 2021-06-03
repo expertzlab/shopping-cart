6fb78fe035ce8e1cd0f3211d555c6eeb39cef7cc
Gireesh Babu <gireesh.babu@gofreelab.com>
2021-06-03 10:06:35 +0530

    form template solutions:

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-cart';
+  defulatEmail = "user@example.com"
+  @ViewChild('userlogin') sform!: NgForm;
+  secret = '';
+  genders = ['Male', 'Female']
+
+  user: User = {email:'', password:'', secret:''}
+
+  constructor(){
+  
+  }
+
+  /*
+  onClickSubmit(form: NgForm){
+    console.log("submitted")
+    console.log(form.value)
+  }
+  */
+
+  onClickSubmit(){
+    console.log(this.sform)
+    
+    this.user.email = this.sform.value.userData.email;
+    this.user.password = this.sform.value.userData.password
+    this.user.secret = this.sform.value.secret
+    this.sform.reset()
+    
+  }
+
+  suggest(){
+    this.sform.form.patchValue({
+      userData:{
+        email: 'super@s.com'
+      }
+    })
+  }
 }
