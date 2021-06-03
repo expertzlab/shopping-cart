2fe391fb6ba75ebb9cc9ca999e109818f58ad55c
Gofreelab <gireesh.babu@gofreelab.com>
2021-06-03 12:37:29 +0530

    sumbit form properties are displayed on the console

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-cart';
+
+
+  email:string = ''
+  password:string = ''
+  secret:string = ''
+
+  constructor() {}
+
+  onClickSubmit(form:NgForm){
+    console.log("submitted")
+    console.log(form)
+  }
+
 }
